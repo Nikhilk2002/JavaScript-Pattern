d07b6502576c857502ef9b982d5db5a80a967543
Nikhilk2002 <nikhilmkd1523@gmail.com>
2024-07-13 19:54:55 +0530

    Square Star

---
+++ Pattern18.js
@@ -0,0 +1,18 @@
+function squareStar(row){
+    let star;
+    for (let i=1;i<=row;i++){
+        star=""
+        for(let j=1;j<=row;j++){
+            if(i===1 || j===1 ||i===row || j===row ){
+                star+="* "
+            }
+            else{
+                star+="  ";
+            }
+           
+        }
+        console.log(star);
+    }
+}
+
+squareStar(6)
