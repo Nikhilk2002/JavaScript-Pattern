337527f113aad5d77550131a2f108a2621953b20
Nikhilk2002 <nikhilmkd1523@gmail.com>
2024-07-15 21:59:51 +0530

    Square pattern Include X

---
+++ Pattern20.js
@@ -0,0 +1,18 @@
+const squareStarX= (row)=>{
+    let star;
+    for (let i=1;i<=row;i++){
+        star=""
+        for(let j=1;j<=row;j++){
+            if(i===1 || j===1 ||i===row || j===row || i===j  || i+j===row+1){
+                star+="* "
+            }
+            else{
+                star+="  ";
+            }
+           
+        }
+        console.log(star);
+    }
+}
+
+squareStarX(6)
