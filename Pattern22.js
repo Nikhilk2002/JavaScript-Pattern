e99dd89d235b1f5813ea7a9ef0965b452f4c500b
Nikhilk2002 <nikhilmkd1523@gmail.com>
2024-07-18 20:36:03 +0530

    / Pattern

---
+++ Pattern22.js
@@ -0,0 +1,19 @@
+const slashFunction=(row)=>{
+    let star;
+    for(let i=1;i<=row;i++){
+        star=' '
+        for(let j=1;j<=row;j++){
+            if(i+j===row+1){
+                star+="* "
+            }
+            else{
+                star+=" "
+            }
+           
+        }
+        console.log(star);
+    }
+
+}
+
+slashFunction(6)
